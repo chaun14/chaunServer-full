c65b2628bdb69c083639a7780acf4c5527e9841e
chaun14 <chaun1403@gmail.com>
2021-04-05 13:10:25 +0200

    Allow mysql port config

--- db/index.js
+++ db/index.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
+  port: dbConfig.port,
 
   define: {
     charset: "utf8mb4",
@@ -14,10 +15,10 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   },
 
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   },
 });
 
